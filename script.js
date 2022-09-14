53575a2be66ec0733648551847d35c96dfc3e4a3
NiyaRakhewar <niharikarakhewar@gmail.com>
2022-09-14 23:25:23 +0530

    updtae

---
+++ script.js
@@ -0,0 +1,11 @@
+function reverseStr(str){
+    var listOfChars = str.split('');;
+    var reverseListOfChars = listOfChars.reverse();
+
+    var reverseStr =  reverseListOfChars.join('');
+    
+
+    return reverseStr;
+}
+
+console.log(reverseStr('hello'));
